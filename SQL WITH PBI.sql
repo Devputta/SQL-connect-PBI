81dd5e2f6896da0c08f06d103333896e22a1eff1
Mahadevu_M_P <143114779+Devputta@users.noreply.github.com>
2025-11-02 21:29:02 +0530

    📦 SQL to Power BI — Data Analytics Project
    This project demonstrates how to connect a MySQL database to Power BI and create interactive dashboards for sales analytics.

---
+++ SQL WITH PBI.sql
@@ -0,0 +1,27 @@
+show databases;
+
+USE `awesome chocolates`;
+
+show tables;
+
+select * from geo;
+select * from people;
+select * from products;
+select * from sales;
+
+select * from sales 
+where GeoID ="G4" and Amount > 5000;
+
+select s.* from sales s join geo g on s.GeoID = g.GeoID 
+where g.Geo = "Canada";
+
+select s.*, g.* from sales s left join geo g on s.GeoID = g.GeoID;
+
+select s.*, g.* from sales s right join geo g on s.GeoID = g.GeoID;
+
+select s.*, g.* from sales s straight_join  geo g on s.GeoID = g.GeoID;
+
+
+select * from sales order by Boxes;
+
+select * from sales where Boxes < 50;
